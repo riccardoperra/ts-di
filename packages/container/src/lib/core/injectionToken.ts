d79d288ba70dd2867e6a41edabb9518a68ebce60
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 19:43:36 +0200

    doc: added injection token documentation and example, changed file name to camelCase

---
+++ packages/container/src/lib/core/injectionToken.ts
@@ -0,0 +1,29 @@
+/**
+ * Creates an injection token that could be used in the DI Container.
+ *
+ * `Injection Token` has a generic T which is the type of object which will
+ * be returned by the container injector.
+ *
+ * ``` typescript
+ * type MyType = {...}
+ * const myToken = injector.get(InjectionToken.of<MyType>('desc');
+ *
+ * ```
+ */
+export class InjectionToken<T> {
+  protected constructor(private _desc: string) {
+  }
+
+  toString() {
+    return `[ts-di/token] ${this._desc}`;
+  }
+
+  /**
+   * @description
+   * Create a new instance of InjectionToken with the given description
+   * @param _desc
+   */
+  static of<T = unknown>(_desc: string) {
+    return new InjectionToken<T>(_desc);
+  }
+}
