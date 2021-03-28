8e81fe92d1a2a6336032fdb62a6855b716f68268
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 22:50:09 +0200

    added exports

--- packages/container/src/index.ts
+++ packages/container/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/container';
+export {Token} from './lib/interface/token';
+export {DIProvider} from './lib/interface/provider';
+
+export {StaticInjector} from './lib/core/injector';
