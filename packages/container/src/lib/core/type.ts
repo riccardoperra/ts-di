c08e7ab1f447bb6c2e3003e3d3bed4d652fad813
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 19:47:04 +0200

    feat: added typed function interface and helpers

---
+++ packages/container/src/lib/core/type.ts
@@ -0,0 +1,11 @@
+export const Type = Function;
+
+export const isType = <T>(obj: T): boolean => typeof obj === 'function';
+
+/**
+ * Used internally to infer a function type with generics
+ * @internal
+ */
+export interface Type<T> extends Function {
+  new(...args: any[]): T[]
+}
