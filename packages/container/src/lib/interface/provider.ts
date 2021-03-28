b353dc9d9588deb99bcc07746c7365cc7ea8588a
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 22:41:07 +0200

    types: added GenericProvider interface, added provider type exports

--- packages/container/src/lib/interface/provider.ts
+++ packages/container/src/lib/interface/provider.ts
@@ -1,11 +1,19 @@
-import { Type } from '../core/type';
+import type { Type } from '../core/type';
 import { Token } from './token';
+import { Observable } from './observable';
 
+export interface GenericProvider<T = any> {
+  provide: Token<T>;
+  useValue?: T;
+  useClass?: Type<T>;
+  useFactory?: (...args: any[]) => T;
+  deps?: []
+}
 /**
  * Define a provider that will be resolved returning the `useValue` property value.
  * @internal
  */
-interface ValueProvider<T> {
+export interface ValueProvider<T> {
   /**
    * The injection token. It should be the instance of InjectionToken or a string that will
    * be used during the injection process to resolve the value by the injector.
@@ -15,13 +23,18 @@ interface ValueProvider<T> {
    * The value to inject.
    */
   useValue: T;
+
+  deps?: any[]
 }
 /**
  * Define a provider that will be resolved invoking a `useFactory` function.
+ * The provider will support the case if the factory function return a promise or
+ * a generic observable. In this last case, the observable must complete.
  */
-interface FactoryProvider<T> {
-  useFactory: (...args: any[]) => T;
-  deps?: []
+export interface FactoryProvider<T> {
+  useFactory: (...args: any[]) => T | Promise<T> | Observable<T>;
+  provide: Token<T>;
+  deps?: any[]
 }
 
 /**
@@ -29,8 +42,9 @@ interface FactoryProvider<T> {
  * the given deps as constructor arguments if provided.
  * @internal
  */
-interface TypeProvider<T> extends Type<T> {
-  deps?: any[]
+export interface TypeProvider<T> extends Type<T> {
+  deps?: any[];
+  provide?: Token<T>;
 }
 
 /**
@@ -38,7 +52,7 @@ interface TypeProvider<T> extends Type<T> {
  * It will return the context instance of given useClass param
  * @internal
  */
-interface ClassProvider<T> {
+export interface ClassProvider<T> {
   /**
    * The injection token. It should be the instance of InjectionToken or a string that will
    * be used during the injection process to resolve the value by the injector.
@@ -63,7 +77,7 @@ interface ClassProvider<T> {
  * Define the provider configuration type that will be used by the injector
  * @publicApi
  */
-export type DIProvider<T> =
+export type DIProvider<T = any> =
   | TypeProvider<T>
   | ClassProvider<T>
   | FactoryProvider<T>
