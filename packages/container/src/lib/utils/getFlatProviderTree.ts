3d14c2e92644b5ec3b08bc9761dc463a718972b9
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 22:41:49 +0200

    feat: added getFlatProviderTree utility

---
+++ packages/container/src/lib/utils/getFlatProviderTree.ts
@@ -0,0 +1,13 @@
+import { DIProvider } from '../interface/provider';
+import { pipe } from 'fp-ts/function';
+import { fold, fromNullable } from 'fp-ts/Option';
+import { Injector, safeGetProvider } from '../core/injector';
+import { mergeProviders } from './mergeProvider';
+
+export const getFlatProviderTree = (c: Injector): DIProvider[] => pipe(
+  fromNullable(c.parent),
+  fold(
+    () => safeGetProvider(c),
+    parent => mergeProviders(safeGetProvider(c))(safeGetProvider(parent))
+  )
+);
