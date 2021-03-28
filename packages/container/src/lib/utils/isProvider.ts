434b0a0a58dde8996f7f620f6a35e9b758f46812
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-28 22:39:01 +0200

    feat: added isProvider utility helper

---
+++ packages/container/src/lib/utils/isProvider.ts
@@ -0,0 +1,6 @@
+import { DIProvider } from '../interface/provider';
+import { Token } from '../interface/token';
+
+export const isProvider = <T extends DIProvider & { provide?: Token<any>}>(
+  providerLike: T
+) => providerLike.provide !== undefined;
