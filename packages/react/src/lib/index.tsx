f2a9089f609aebd0d26c7442bd459ac4c2b7f0ea
Riccardo Perra <riccardo.perra@icloud.com>
2021-03-29 20:56:52 +0200

    addd Ã@ts-di/react

---
+++ packages/react/src/lib/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface ReactProps {}
+
+export function Index(props: ReactProps) {
+  return (
+    <div>
+      <h1>Welcome to react!</h1>
+    </div>
+  );
+}
+
+export default Index;
